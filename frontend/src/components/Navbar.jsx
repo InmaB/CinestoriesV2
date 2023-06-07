34a4b33e8575584de76b433b2909f5ac3fea08c1
InmaB <inmabalbuena@gmail.com>
2023-06-07 21:26:50 +0200

    menu responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,353 +1,3 @@
-// import React, { useState } from 'react';
-// import styled from "styled-components";
-// import logo from "../assets/logo.png";
-// import { onAuthStateChanged } from "firebase/auth";
-// import { Link, useNavigate } from "react-router-dom";
-// import { AiFillHome, AiOutlineCheckCircle, AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-// import { BiCameraMovie } from "react-icons/bi";
-// import { MdFavorite } from "react-icons/md";
-// import { SlScreenDesktop } from "react-icons/sl";
-// import { firebaseAuth } from "../utils/firebase-config";
-// import UserProfile from './UserProfile';
-
-// export default function Navbar() {
-//   // Se declara funciones propias de react
-//   const navegacion = useNavigate();
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   // Matriz de enlaces del menú
-//   const links = [
-//     { ico: AiFillHome, name: "Inicio", link: "/" },
-//     { ico: BiCameraMovie, name: "Pelis", link: "/peliculas" },
-//     { ico: SlScreenDesktop, name: "Series & Tv", link: "/seriestv" },
-//     { ico: MdFavorite, name: "Favoritas", link: "/listaFavoritas" },
-//     { ico: AiOutlineCheckCircle, name: "Pendientes", link: "/listaPendientes" },
-//   ];
-
-//   // Verifica si hay un usuario autenticado.
-//   onAuthStateChanged(firebaseAuth, (Usuario) => {
-//     if (!Usuario) navegacion("/login");
-//   });
-
-//   const toggleMenu = () => {
-//     console.log('Toggle menu');
-//     setIsOpen(!isOpen);
-//   };
-
-
-//   return (
-//     <Nav isOpen={isOpen}>
-//       <div className="flex a-center">
-//         <div className="logo">
-//           <a href='/' >
-//             <img src={logo} alt="logo" />
-//           </a>
-//         </div>
-//         <div className="menu-icon" onClick={toggleMenu}>
-//           {isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
-//         </div>
-
-//         <ul className="menu">
-//           {links.map(({ ico: Icon, name, link }) => (
-//             <li key={name}>
-//               <Link to={link}>
-//                 {Icon && <Icon />} {name}
-//               </Link>
-//             </li>
-//           ))}
-//         </ul>
-
-//         <div className="dcha">
-//           <UserProfile></UserProfile>
-//           <div className="buscador">
-//           </div>
-//         </div>
-//       </div>
-//     </Nav>
-//   );
-// }
-
-// // Estilos
-// const Nav = styled.nav`
-//   box-shadow: 0 3px 3px black;
-//   top: 0;
-//   background-color: #1a1d29;
-//   height: 4.5rem;
-//   width: 100%;
-//   z-index: 2;
-//   padding: 0.3rem;
-//   justify-content: space-between;
-//   align-items: center;
-
-//     .logo {
-//       display: flex;
-//       align-items: center;
-//       justify-content: center;
-//       img {
-//         margin-left: 2rem;
-//         margin-right:2rem;
-//         height: 4rem;
-//       }
-//     }
-
-//   .menu-icon {
-//     font-size: 24px;
-//     cursor: pointer;
-
-//      @media (min-width: 768px) {
-//        display: none;
-//     }
-//   }
-
-//   .menu {
-//     display: flex;
-//     align-items: center;
-
-//     @media (max-width: 767px) {
-//       display: block;
-//       position: absolute;
-//       top: 4.5rem;
-//       left: 0;
-//       width: 100%;
-//       background-color: #1a1d29;
-//       padding: 1rem;
-//       visibility: hidden;
-//       opacity: 0;
-//       transition: opacity 0.2s ease-in;
-
-//       &.open {
-//         visibility: visible;
-//         opacity: 1;
-//       }
-
-//       ul {
-//         li {
-//           margin-bottom: 0.5rem; // Reduce el espacio entre los elementos de la lista
-//         }
-
-//         a {
-//           display: block; // Muestra los enlaces en forma de lista vertical
-//           padding: 0.5rem 0; // Añade espaciado vertical a los enlaces
-//         }
-//       }
-//      }
-
-//     li {
-//       list-style: none;
-//       margin-left: 20px;
-//       margin: 0.8rem;
-//       text-transform: uppercase;
-//       font-size:1.1rem;
-
-//       a {
-//         position: relative;
-//         overflow: hidden;
-//         display: inline-block;
-//         text-decoration: none;
-//         color: white;
-//         transition: color 0.2s ease-in;
-//       }
-
-//       a:hover {
-//   color: lime;
-// }
-
-// a:after {
-// content: "";
-// position: absolute;
-// bottom: 0;
-// left: -100%;
-// width: 100%;
-// height: 2px;
-// background: lime;
-// transition: left .4s;
-// }
-// a:hover:after {
-// left: 0;
-// }
-//     }
-//   }
-// .dcha {
-//   margin-left: auto;
-//   margin-right:2rem;
-// }
-
-// `;
-
-
-// import React, { useState } from 'react';
-// import styled from "styled-components";
-// import logo from "../assets/logo.png";
-// import { onAuthStateChanged } from "firebase/auth";
-// import { Link, useNavigate } from "react-router-dom";
-// import { AiFillHome, AiOutlineCheckCircle, AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-// import { BiCameraMovie } from "react-icons/bi";
-// import { MdFavorite } from "react-icons/md";
-// import { SlScreenDesktop } from "react-icons/sl";
-// import { firebaseAuth } from "../utils/firebase-config";
-// import UserProfile from './UserProfile';
-
-// export default function Navbar() {
-//   const navegacion = useNavigate();
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const links = [
-//     { ico: AiFillHome, name: "Inicio", link: "/" },
-//     { ico: BiCameraMovie, name: "Pelis", link: "/peliculas" },
-//     { ico: SlScreenDesktop, name: "Series & Tv", link: "/seriestv" },
-//     { ico: MdFavorite, name: "Favoritas", link: "/listaFavoritas" },
-//     { ico: AiOutlineCheckCircle, name: "Pendientes", link: "/listaPendientes" },
-//   ];
-
-//   onAuthStateChanged(firebaseAuth, (Usuario) => {
-//     if (!Usuario) navegacion("/login");
-//   });
-
-//   const toggleMenu = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <Nav>
-//       <div className="flex a-center">
-//         <div className="logo">
-//           <a href="/">
-//             <img src={logo} alt="logo" />
-//           </a>
-//         </div>
-//         <div className={"menu-icon " + (isOpen ? 'open' : '')} onClick={toggleMenu}>
-//           {isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
-//         </div>
-//         <ul className={"menu " + (isOpen ? 'open' : '')}>
-//           {links.map(({ ico: Icon, name, link }) => (
-//             <li key={name}>
-//               <Link to={link}>
-//                 {Icon && <Icon />} {name}
-//               </Link>
-//             </li>
-//           ))}
-//         </ul>
-//         <div className="dcha">
-//           <UserProfile></UserProfile>
-//         </div>
-//       </div>
-//     </Nav>
-//   );
-// }
-
-// const Nav = styled.div`
-//   box-shadow: 0 3px 3px black;
-//   top: 0;
-//   background-color: #1a1d29;
-//   height: 4.5rem;
-//   width: 100%;
-//   z-index: 2;
-//   padding: 0.3rem;
-//   justify-content: space-between;
-//   align-items: center;
-
-//   .logo {
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//     img {
-//       margin-left: 2rem;
-//       margin-right: 2rem;
-//       height: 4rem;
-//     }
-//   }
-
-//   .menu-icon {
-//     font-size: 24px;
-//     cursor: pointer;
-
-//     @media (min-width: 768px) {
-//       display: none;
-//     }
-//   }
-
-//   .menu {
-//     display: flex;
-//     align-items: center;
-
-//     li {
-//       list-style: none;
-//       margin-left: 20px;
-//       margin: 0.8rem;
-//       text-transform: uppercase;
-//       font-size: 1.1rem;
-
-//       a {
-//         position: relative;
-//         overflow: hidden;
-//         display: inline-block;
-//         text-decoration: none;
-//         color: white;
-//         transition: color 0.2s ease-in;
-
-//         &:hover {
-//           color: lime;
-//         }
-
-//         &:after {
-//           content: "";
-//           position: absolute;
-//           bottom: 0;
-//           left: -100%;
-//           width: 100%;
-//           height: 2px;
-//           background: lime;
-//           transition: left 0.4s;
-//         }
-
-//         &:hover:after {
-//           left: 0;
-//         }
-//       }
-//     }
-//   }
-
-//   .dcha {
-//     margin-left: auto;
-//     margin-right: 2rem;
-//   }
-
-//   @media (max-width: 767px) {
-//     .menu.open {
-//       display: block;
-//       position: absolute;
-//       top: 4.5rem;
-//       left: 0;
-//       width: 100%;
-//       background-color: #1a1d29;
-//       padding: 1rem;
-//       visibility: hidden;
-//       opacity: 0;
-//       transition: opacity 0.2s ease-in;
-
-//       &.open {
-//         visibility: visible;
-//         opacity: 1;
-//         z-index: 9999;
-//       }
-
-//       ul {
-//         flex-direction: column;
-//         align-items: flex-start;
-
-//         li {
-//           margin-bottom: 0.5rem;
-//         }
-
-//         a {
-//           display: block;
-//           padding: 0.5rem 0;
-//         }
-//       }
-//     }
-//   }
-// `;
-
 import React, { useState } from 'react';
 import styled from "styled-components";
 import logo from "../assets/logo.png";
@@ -361,9 +11,14 @@ import { firebaseAuth } from "../utils/firebase-config";
 import UserProfile from './UserProfile';
 
 export default function Navbar() {
+
+  // Se declara funciones propias de react
   const navegacion = useNavigate();
+
+  // Se utiliza useState para declarar múltiples variables de estado
   const [isOpen, setIsOpen] = useState(false);
 
+  // Se declara una matriz de links
   const links = [
     { ico: AiFillHome, name: "Inicio", link: "/" },
     { ico: BiCameraMovie, name: "Pelis", link: "/peliculas" },
@@ -372,10 +27,12 @@ export default function Navbar() {
     { ico: AiOutlineCheckCircle, name: "Pendientes", link: "/listaPendientes" },
   ];
 
+  // Verifica si hay un usuario autenticado
   onAuthStateChanged(firebaseAuth, (Usuario) => {
     if (!Usuario) navegacion("/login");
   });
 
+  // Función para cambiar el estado isOpen cuando se hace clic en el botón del menu responsive
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
@@ -388,19 +45,24 @@ export default function Navbar() {
             <img src={logo} alt="logo" />
           </a>
         </div>
+        {/* Aplica estilos específicos cuando isOpen es true, para mostrar un ícono de cierre y también llama a toggleMenu donde cambia el estado  */}
         <div className={"menu-icon " + (isOpen ? 'open' : '')} onClick={toggleMenu}>
+          {/* Icono del botón del menu responsive */}
           {isOpen ? <AiOutlineClose /> : <AiOutlineMenu />}
         </div>
         <ul className={"menu " + (isOpen ? 'open' : '')}>
+          {/* Mapea la matriz de links */}
           {links.map(({ ico: Icon, name, link }) => (
             <li key={name}>
               <Link to={link}>
+                {/* Icono y nombre del elemento de menú */}
                 {Icon && <Icon />} {name}
               </Link>
             </li>
           ))}
         </ul>
         <div className="dcha">
+          {/* Componente UserProfile */}
           <UserProfile></UserProfile>
         </div>
       </div>
@@ -408,6 +70,7 @@ export default function Navbar() {
   );
 }
 
+// Estilos
 const Nav = styled.div`
   box-shadow: 0 3px 3px black;
   top: 0;
@@ -443,6 +106,16 @@ const Nav = styled.div`
   .menu {
     display: flex;
     align-items: center;
+
+    @media (max-width: 767px) {
+      display: none;
+    }
+
+    @media (min-width: 767px) and (max-width: 900px) {
+      li a {
+        font-size: 0.8rem;
+      }
+    }
 
     li {
       list-style: none;
@@ -521,4 +194,3 @@ const Nav = styled.div`
     }
   }
 `;
-
